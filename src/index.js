d1c97a829693761282f649e26fd1c9f7cd7049bc
Mindaugas Kapickas <mindaugas@mkp.com>
2021-05-24 10:38:44 +0300

    update viska

---
+++ src/index.js
@@ -0,0 +1,4 @@
+"use strict";
+import greet from "./js/greet.js";
+
+console.log("greet fn", greet("its fokin monday mate aay."));
---
+++ src/js/greet.js
@@ -0,0 +1,3 @@
+export default function greet(say) {
+  return "Bob says " + say;
+}
