c5a93be3b31749a90d43aad50bfcc36289955c6c
Mindaugas Kapickas <mindaugas@mkp.com>
2021-05-24 13:10:31 +0300

    updated

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,6 @@
 const path = require("path"); //node modulis dirbti su keliais iki failu
+//html generavimo pluginas
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -8,5 +10,17 @@ module.exports = {
   },
   output: {},
   module: {},
-  plugins: [],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/html/template.html",
+      templateParameters: {
+        title: "Now we know webpack.",
+        mainTitle: "Thats the heading of main title",
+      },
+      minify: {
+        removeComments: true,
+        collapseWhitespace: false,
+      },
+    }),
+  ],
 };
